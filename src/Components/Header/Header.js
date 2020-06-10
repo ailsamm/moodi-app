5236812213915e1b2255a9e22737bc2efae7e9ea
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-10 09:49:53 -0700

    Update header buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { ButtonGroup } from '@material-ui/core';
 import './Header.css';
 
 export default class Header extends Component {
@@ -9,10 +10,10 @@ export default class Header extends Component {
                 <NavLink to="/" className="header__navLink">
                     <h1 className="header__moodi">moodi</h1>
                 </NavLink>
-                <div className="header__buttons">
+                <ButtonGroup className="header__buttons" variant="text" orientation="vertical" aria-label="outlined secondary button group">
                     <NavLink to="/signup" className="button header__signUpButton">sign up</NavLink>
                     <NavLink to="/login" className="button header__logInButton">log in</NavLink>
-                </div>
+                </ButtonGroup>
             </div>
         )
     }
