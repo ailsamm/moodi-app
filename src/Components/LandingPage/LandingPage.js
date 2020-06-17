500fb2be303adb8fce3f0c2f89b968c284f21d59
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-17 16:49:05 -0700

    Style landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Bounce from 'react-reveal';
 import './LandingPage.css';
 
 export default class LandingPage extends Component {
     
     render() {
         return (
-            <div className="landingPage centeredContent">
-                <p className="landingPage__meet">welcome to</p>
-                <h1 className="landingPage__title">moodi</h1>
-                <p className="landingPage__description">your best friend in mood tracking</p>
-                <NavLink to="/signup" className="button landingPage__learnMorebutton">sign up</NavLink>
+            <div className="landingPage centered background">
+                    <p className="landingPage__meet">welcome to</p>
+                    <Bounce>
+                        <h1 className="landingPage__title">moodi</h1>
+                    </Bounce>
+                    <p className="landingPage__description">your best friend in mood tracking</p>
+                    <NavLink to="/signup" className="button landingPage__learnMorebutton">sign up</NavLink>
+                <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
             </div>
         );
     }
